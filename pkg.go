4f9d657fa03dfb2e1daa462a59d1636dae19b87d
Adam Malcontenti-Wilson <amalcontenti-wilson@zendesk.com>
2018-07-13 16:30:35 -0700

    working?

--- pkg.go
+++ pkg.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	//
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -10,11 +9,15 @@ import (
 
 	"github.com/jessevdk/go-flags"
 
+	"bytes"
 	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/zendesk/kinesiscat/kpl"
 	"os"
 )
 
-//var MAGIC = []byte("\xf3\x89\x9a\xc2")
+var MAGIC = []byte("\xf3\x89\x9a\xc2")
+var NEWLINE = []byte{'\n'}
 
 type Options struct {
 	Region string `long:"region" description:"AWS Region" required:"true" env:"AWS_REGION"`
@@ -58,6 +61,48 @@ func getShardIds(client kinesisiface.KinesisAPI, streamName string) (shardIds []
 	return
 }
 
+func getShardIterators(client kinesisiface.KinesisAPI, streamName string, shardIds []string) (shardIterators []string) {
+	shardIterators = []string{}
+	shardIteratorType := kinesis.ShardIteratorTypeLatest
+	for _, shardId := range shardIds {
+		iterator, err := client.GetShardIterator(&kinesis.GetShardIteratorInput{
+			ShardId:           &shardId,
+			ShardIteratorType: &shardIteratorType,
+			StreamName:        &streamName,
+		})
+		fatalfIfErr("get iterator error: %v", err)
+		shardIterators = append(shardIterators, *iterator.ShardIterator)
+	}
+	return
+}
+
+func streamRecords(client kinesisiface.KinesisAPI, shardIterator string, stream *os.File) {
+	params := kinesis.GetRecordsInput{
+		ShardIterator: &shardIterator,
+	}
+	resp, err := client.GetRecords(&params)
+	fatalfIfErr("get records error: %v", err)
+	MD5_SIZE := 16
+	for _, record := range resp.Records {
+		// see https://github.com/awslabs/amazon-kinesis-producer/blob/master/aggregation-format.md
+
+		is_aggregated := bytes.Compare(record.Data[0:len(MAGIC)], MAGIC) == 0
+
+		if is_aggregated {
+			agg := &kpl.AggregatedRecord{}
+			err := proto.Unmarshal(record.Data[len(MAGIC):len(record.Data)-MD5_SIZE], agg)
+			fatalfIfErr("protobuf unmarshal error: %v", err)
+			for _, r := range agg.Records {
+				stream.Write(r.Data)
+				stream.Write(NEWLINE)
+			}
+		} else {
+			stream.Write(record.Data)
+			stream.Write(NEWLINE)
+		}
+	}
+}
+
 func main() {
 	var opts Options
 	_, err := flags.ParseArgs(&opts, os.Args[1:])
@@ -67,7 +112,10 @@ func main() {
 
 	var client = buildKinesisClient(opts.Region)
 	var shardIds = getShardIds(client, opts.Args.StreamName)
-	log.Print(shardIds)
+	var shardIterators = getShardIterators(client, opts.Args.StreamName, shardIds)
+	for _, shardIterator := range shardIterators {
+		streamRecords(client, shardIterator, os.Stdout)
+	}
 
 	//ctx, cancel := context.WithCancel(ctx)
 	//defer cancel()
